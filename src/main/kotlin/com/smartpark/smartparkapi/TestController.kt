d0854b628904e2151a2a29b518b2f131e4b8e136
Raghu Kalyan <raghukv11188@gmail.com>
2022-09-18 12:10:56 +0530

    smartpark API first commit

---
+++ src/main/kotlin/com/smartpark/smartparkapi/TestController.kt
@@ -0,0 +1,15 @@
+package com.smartpark.smartparkapi
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@EnableAutoConfiguration
+class TestController {
+
+    @GetMapping("/test")
+    fun getTest(): String{
+        return "hello"
+    }
+}
---
+++ src/test/kotlin/com/smartpark/smartparkapi/SmartparkapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smartpark.smartparkapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartparkapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
